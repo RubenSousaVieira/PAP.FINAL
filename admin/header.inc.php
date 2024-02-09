a7262d4c8a59854ff96da2f4fb65a043340b777c
Rúben Vieira <110641459+RubenSousaVieira@users.noreply.github.com>
2024-02-09 10:05:49 +0000

    Add files via upload

--- admin/header.inc.php
+++ admin/header.inc.php
@@ -9,7 +9,7 @@ include_once("check_session.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>yRepair</title>
+    <title>Área de Administração</title>
     <!-- jQuery -->
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js"></script>
     <!-- Bootstrap -->
